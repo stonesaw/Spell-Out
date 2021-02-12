aec2cc1d327355f734f586ecf2dcfc09cc0fb0f9
stonesaw <ishichan1260@gmail.com>
2021-02-12 19:15:46 +0900

    Add: ScrollablePage

---
+++ lib/utils/scrollable_page.rb
@@ -0,0 +1,69 @@
+class ScrollablePage < RenderTarget
+  attr_reader :pos
+
+  def initialize(width, height, 
+                 page_width=nil, page_height=nil, bgcolor=[0, 0, 0, 0], 
+                 scrollbar: nil, scrollbar_base: nil,
+                 bar_w: 16, bar_color: [200, 200, 200], bar_base_color: C_WHITE
+                )
+    super(width, height, bgcolor)
+    @page_width = page_width || self.width
+    @page_height = page_height || self.height
+
+    @bar_h_per = [self.height.to_f / @page_height, 1].min
+    @scrollbar = scrollbar || Image.new(bar_w, (self.height * @bar_h_per).to_i, bar_color)
+    @scrollbar_base = scrollbar_base || Image.new(bar_w, self.height, bar_base_color)
+    
+    @pos = 0
+    @_before_mouse_wheel = Input.mouse_wheel_pos
+  end
+
+  def draw_scrollbar
+    self.draw(self.width - @scrollbar_base.width, @pos, @scrollbar_base)
+    self.draw(self.width - @scrollbar.width, @pos * @bar_h_per + @pos, @scrollbar)
+  end
+
+  def update
+    scroll_volume = (@_before_mouse_wheel - Input.mouse_wheel_pos) / 120
+    scroll_volume *= 50
+    @_before_mouse_wheel = Input.mouse_wheel_pos
+
+    @pos += scroll_volume
+    @pos = [0, [@pos, @page_height - self.height].min].max
+  end
+
+  # wrapping methods
+  [
+    'draw(x, y,image, z=0)',
+    'draw_scale(x, y, image, scale_x, scale_y, center_x=nil, center_y=nil, z=0)',
+    'draw_rot(x, y, image, angle, center_x=nil, center_y=nil, z=0)',
+    'draw_alpha(x, y, image, alpha, z=0)',
+    'draw_add(x, y, image, z=0)',
+    'draw_sub(x, y, image, z=0)',
+    'draw_shader(x, y, image, shader, z=0)',
+    'draw_ex(x, y, image, option={})',
+    'draw_font(x, y, text, font, option={})',
+    'draw_font_ex(x, y, text, font, option={})',
+    'draw_morph(x1, y1, x2, y2, x3, y3, x4, y4, image, option={})',
+    'draw_tile(base_x, base_y, map, image_array, start_x, start_y, size_x, size_y, z=0)',
+    'draw_pixel(x, y, color, z=0)',
+    'draw_line(x1, y1, x2, y2, color, z=0)',
+    'draw_box(x1, y1, x2, y2, color, z=0)',
+    'draw_box_fill(x1, y1, x2, y2, color, z=0)',
+    'draw_circle(x, y, r, color, z=0)',
+    'draw_circle_fill(x, y, r, color, z=0)'
+  ].each do |method|
+    match = method.match(/^(\w*)\((.+)\)$/)
+    name = match[1]
+    arg = match[2]
+    # puts "#{name}(#{arg})"
+    new_arg = arg
+      .gsub(/(\w+)\s*=.+?,/, "\\1,")
+      .sub(/(\w+)\s*=.+?$/, "\\1")
+      .gsub(/(x|x\d), (y|y\d),/, "\\1, \\2 - @pos,")
+    # puts "#{' ' * (name.length - 2)}=> #{new_arg}"
+    eval "def #{name}(#{arg})
+            super(#{new_arg})
+          end"
+  end
+end
---
+++ playground/scrollable_page_test.rb
@@ -0,0 +1,56 @@
+require 'dxruby'
+require_relative '../lib/utils/scrollable_page'
+
+Font.install('MonospaceBold.ttf')
+
+page = ScrollablePage.new(500, 400, 500, 800, [34, 34, 34])
+
+font = Font.new(24)
+font_code = Font.new(21, 'Monospace')
+
+
+usage = "
+page = ScrollablePage.new(
+  500, 400, 500, 800, [34, 34, 34]
+)
+
+Window.loop do
+  break if Input.key_down? K_ESCAPE
+  
+  page.update
+  
+  page.draw_font(10, 550, \'Sample Text\', font)
+  
+  page.draw_scrollbar
+  Window.draw(70, 40, page)
+end
+".split(/\R/)
+
+
+a = Sprite.new(100, 0, Image.new(50, 50, C_RED))
+a.target = page
+
+Window.loop do
+  break if Input.key_down? K_ESCAPE
+  
+  page.update
+
+  page.draw_font(10, 10, "FPS : #{Window.real_fps}", font)
+  page.draw_font(10, 34, 'class ScrollablePage', font)
+  page.draw_font(10, 60, 'マウスのスクロールで動かせます', font)
+  usage.length.times do |i|
+    page.draw_font(10, 140 + font_code.size*i, usage[i], font_code)
+  end
+  page.draw_font(10, 550, 'result ... ', font)
+  page.draw_font(10, 600, 'Sample Text', font)
+  str = '© stonesaw'
+  page.draw_font((page.width - font.get_width(str)) / 2, 800 - 34, str, font)
+
+  a.y = 650 - page.pos
+  a.draw
+
+  page.draw_scrollbar
+  Window.draw(70, 40, page)
+
+  Window.draw_font(10, 10, "#{page.pos}", font)
+end
